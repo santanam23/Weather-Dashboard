35084f5fafe0fcfadaf8229d7f5c8299b494aab2
santanam23 <mirmir11.ms@gmail.com>
2022-01-02 14:00:05 -0600

    created js script

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var apiKey = "ae4efa771bec445ae7940b0febb33e9c";
+
+var today = moment().format('L');
+
+var searchHistoryList = [];
+
+function currentCondition(city) {
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+
+}
+
